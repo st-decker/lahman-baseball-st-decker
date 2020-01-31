09372ce778e2287132af192b987d0d911d6f4f58
st-decker <37890176+st-decker@users.noreply.github.com>
2020-01-30 21:03:23 -0600

    Simple query to answer Q. Revisit

--- queries/1_question.sql
+++ queries/1_question.sql
@@ -1,1 +1,26 @@
-asdfas
+/*
+    QUESTION ::
+        What range of years does the provided database cover?
+
+    SOURCES ::
+        * ...
+
+    DIMENSIONS ::
+        * ...
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        ...
+
+*/
+
+SELECT MAX(yearid) AS recent_year, MIN(yearid) AS earliest_year, MAX(yearid) - MIN(yearid) AS years_covered
+FROM appearances;
