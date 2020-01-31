178a2a9865278efeae356bc9b1b316d2ce151a53
st-decker <37890176+st-decker@users.noreply.github.com>
2020-01-30 20:30:20 -0600

    Test

---
+++ queries/1_question_answer.sql
@@ -0,0 +1,26 @@
+/*
+    QUESTION ::
+        What range of years does the provided database cover?
+
+    SOURCES ::
+        Appearances
+
+    DIMENSIONS ::
+        
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        1871-2016
+
+*/
+
+SELECT MIN(yearid), MAX(yearid)
+FROM appearances
