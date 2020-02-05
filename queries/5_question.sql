fc6f5bc3bd8f1996683fb71de51ed466093a0c42
st-decker <37890176+st-decker@users.noreply.github.com>
2020-02-04 18:09:16 -0600

    Question 5 initial commit

---
+++ queries/5_question.sql
@@ -0,0 +1,28 @@
+/*
+    QUESTION ::
+        Find the average number of strikeouts per game by decade since 1920.
+		Round the numbers you report to 2 decimal places. 
+		Do the same for home runs per game. Do you see any trends?
+
+    SOURCES ::
+        * batting
+
+    DIMENSIONS ::
+        * ...
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        ...
+
+*/
+
+SELECT avg(so)
+FROM batting;
